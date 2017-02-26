bce3e80f5b47a9ca271a70ea554add34f387272f
Hannah77 <867036478@qq.com>
2017-02-26 21:07:29 +0800

    Add files via upload

---
+++ classProgram.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+
+public class classProgram {
+	public class MainClass {
+		public void main(String args[]){
+			
+		}
+	}
+
+	public class LoginForm{
+		private UserDAO dao;
+		public void init(){
+			
+		}
+		public void display(){
+			
+		}
+		public void valitime(){
+			
+		}
+	}
+
+	public class UserHannah{
+		private kfc db;
+		public boolean findUser(String userName,String userPassword){
+			return false;
+			
+		}
+	}
+
+	public class kfc{
+		public Connection getConnection(){
+			return null;
+			
+		}
+	}
+}
